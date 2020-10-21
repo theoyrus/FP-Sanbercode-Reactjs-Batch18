964fb82da0e8ff840a5cdf3f087b126b76927f56
Suryo Prasetyo W <the.oyrus@gmail.com>
2020-10-21 07:29:14 +0700

    init final project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import 'antd/dist/antd.css';
+import Routes from './Route'
+import { AuthProvider } from './AuthContext'
+
+const App = () => {
+  return (
+    <div className="App">
+      <AuthProvider>
+        <Routes></Routes>
+      </AuthProvider>
+    </div >
+  );
+}
+
+export default App;
